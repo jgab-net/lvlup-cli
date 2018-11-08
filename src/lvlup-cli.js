8ae4e4149a4f63371666b19ac36d63deca901594
Abraham José González Barboza <jgab.net@gmail.com>
2018-11-08 02:40:53 -0300

    chore: base commands

--- src/lvlup-cli.js
+++ src/lvlup-cli.js
@@ -1,11 +1,40 @@
-const program = require('commander');
+import yargs from 'yargs';
 
-program
-    .version('0.1.0')
-    .option('-m, --migrate [name]', 'Generate migration with [name]')
-    .parse(process.argv);
-
-if (program.migrate) {
-    console.log('aca');
-    console.log(program.migrate);
-}
+// noinspection BadExpressionStatementJS
+yargs // eslint-disable-line
+  .command({
+    command: 'seed:create <name>',
+    aliases: ['sc'],
+    desc: 'create a seed file',
+    builder: (builder) => {
+      builder
+        .positional('name', {
+          describe: 'name of seed file'
+        });
+    },
+    handler: (argv) => {
+      if (argv.verbose) console.info(`making file :${argv.name}`);
+      console.log(argv.name);
+    }
+  })
+  .command({
+    command: 'seed:up',
+    aliases: ['seed', 'su'],
+    desc: 'run seeds',
+    handler: (argv) => {
+      if (argv.verbose) console.info('running seeds');
+    }
+  })
+  .command({
+    command: 'seed:down',
+    aliases: ['sd'],
+    desc: 'rollback seeds',
+    handler: (argv) => {
+      if (argv.verbose) console.info('rollback seeds');
+    }
+  })
+  .option('verbose', {
+    alias: 'v',
+    default: false
+  })
+  .argv;
